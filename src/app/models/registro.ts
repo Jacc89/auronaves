42a49c6d26b517d50d28c24a993695173f83cfb7
Jacc89 <laflacalex9@gmail.com>
2021-11-28 22:54:39 -0500

    estilos

---
+++ src/app/models/registro.ts
@@ -0,0 +1,30 @@
+export class Empleado{
+    id:number;
+    nombre:string;
+    apellido: string;
+    correo:string;
+    dirreccion:string;
+    password:string;
+    telefono: number;
+    
+   
+  
+    constructor(
+                  id:number,
+                  nombre:string,
+                  apellido: string,
+                  correo:string,
+                  dirreccion:string,
+                  password:string,
+                  telefono: number){
+  
+      this.id = id,
+      this.nombre = nombre,
+      this.apellido = apellido,
+      this.correo = correo,
+      this.dirreccion = dirreccion,
+      this.password = password,
+      this.telefono = telefono
+    }
+  
+  }
